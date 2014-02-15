8fdf06e3b91a513aebde5a66f0d4ede3a6c0bbf5
zhushuaiAtMyPc <bjzhush@gmail.com>
2014-02-15 20:18:19 +0800

    修补将生词添加到已知单词列表的bug

--- deleteNew.php
+++ deleteNew.php
@@ -11,6 +11,7 @@ $delete = explode('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;', $delete);
 $delete = $delete[0];
 
 
+//add to my word
 $resCheck = $mysqli->query('select * from totallist where op_name = "myword"');
 $row = $resCheck->fetch_assoc();
 $wordList = json_decode($row['op_value'], TRUE);
@@ -19,4 +20,12 @@ if (!isset($wordList[$delete])) {
 }
 $jsonValue = json_encode($wordList);
 $mysqli->query(sprintf("update totallist set op_value = '%s' where op_name = 'myword'", $jsonValue));
+
+//remove from totalcount
+$resCheck = $mysqli->query('select * from totallist where op_name = "totalcount"');
+$row = $resCheck->fetch_assoc();
+$wordList = json_decode($row['op_value'], TRUE);
+unset($wordList[$delete]);
+$jsonValue = json_encode($wordList);
+$mysqli->query(sprintf("update totallist set op_value = '%s' where op_name = 'totalcount'", $jsonValue));
 echo 'ok';
